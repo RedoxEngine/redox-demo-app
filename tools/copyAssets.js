7a6a2601c862a0610ccbed608277fab4b4c42ab9
Nijay Patel <nijay@redoxengine.com>
2021-03-27 20:42:56 -0700

    include public assets

--- tools/copyAssets.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var shell = require("shelljs");
-// Copy all the view templates
-shell.cp("-R", "src/views", "dist/");
--- tools/copyAssets.ts
+++ tools/copyAssets.ts
@@ -2,3 +2,4 @@ import * as shell from "shelljs";
 
 // Copy all the view templates
 shell.cp("-R", "src/views", "dist/");
+shell.cp("-R", "src/public", "dist/");
